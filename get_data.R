3d12146fe5823c246850d0428db139cba94aa9a9
Eivind Hammersmark <eivindhammers@gmail.com>
2020-12-16 23:09:19 +0100

    finland out, add new plot

--- get_data.R
+++ get_data.R
@@ -36,8 +36,8 @@ wdnor <- ApiData(07995, ContentsCode = "Dode1", Kjonn = 2:3, Alder = 1:104,
          year >= 2015) %>%
   group_by(week, year) %>%
   summarize(deaths = sum(deaths)) %>%
-  mutate(deaths = ifelse(year == 2020 & week == update_week - 2, deaths / 0.65, deaths),
-         deaths = ifelse(year == 2020 & week == update_week - 3, deaths / 0.85, deaths),
+  mutate(deaths = ifelse(year == 2020 & week == update_week - 2, deaths / 0.75, deaths),
+         deaths = ifelse(year == 2020 & week == update_week - 3, deaths / 0.92, deaths),
          point_type = case_when(year == 2020 & week < update_week - 3 ~ "2020",
                                 year == 2020 & week >= update_week - 3 ~ "2020",
                                 year < 2020 ~ "2015--2019"),
@@ -58,7 +58,7 @@ popswe <- popswe_list %>%
 
 tmp <- tempfile(".xlsx")
 urlswe <- "https://www.scb.se/en/finding-statistics/statistics-by-subject-area/population/population-composition/population-statistics/pong/tables-and-graphs/preliminary-statistics-on-deaths/"
-download.file(urlswe, tmp)
+download.file(urlswe, tmp, mode = "wb")
 
 wdswe <- read_xlsx(tmp, sheet = "Tabell 1", skip = 6) %>%
   select("DagMånad", as.character(2015:2020)) %>%
@@ -105,47 +105,47 @@ wddnk <- GET(urldnk) %>%
   select(year, week, deaths = INDHOLD, point_type, country) %>%
   left_join(popdnk, by = "year") %>%
   mutate(deaths_per100k = 100000 * deaths / population)
-
-#  Finland ----
-urlfin_pop <- "https://pxnet2.stat.fi/PXWeb/api/v1/en/StatFin/vrm/vaerak/statfin_vaerak_pxt_11ra.px"
-reqfin_pop <- '{"query":[{"code":"Alue","selection":{"filter":"item","values":["SSS"]}},{"code":"Tiedot","selection":{"filter":"item","values":["vaesto"]}},{"code":"Vuosi","selection":{"filter":"item","values":["2014","2015","2016","2017","2018","2019"]}}],"response":{"format":"json-stat"}}'
-
-popfin_list <- POST(urlfin_pop, body = reqfin_pop) %>%
-  content("text") %>%
-  fromJSON()
-
-popfin <- cbind(unlist(popfin_list$dataset$dimension$Vuosi$category$label), 
-                popfin_list$dataset$value) %>% 
-  as_tibble(data.frame()) %>%
-  mutate(year = as.integer(V1) + 1, # 31 dec numbers, so assign to next year
-         population = as.numeric(V2)) %>%
-  select(year, population)
-
-urlfin <- "https://pxnet2.stat.fi:443/PXWeb/api/v1/en/Kokeelliset_tilastot/vamuu_koke/statfin_vamuu_pxt_12ng.px"
-
-weeks <- crossing(year = 2015:2020, week = 1:53) %>%
-  mutate(week_fmt = paste(year, str_pad(week, width = 2, pad = "0"), sep = "W")) %>%
-  filter(!(week == 53 & year != 2015)) %>%
-  pull(week_fmt) %>%
-  paste(collapse = '","')
-
-reqfin <- paste0('{"query":[{"code":"Alue","selection":{"filter":"item","values":["SSS"]}},{"code":"Viikko","selection":{"filter":"item","values":["',
-                 weeks,
-                 '"]}},{"code":"Ikä","selection":{"filter":"item","values":["SSS"]}},{"code":"Sukupuoli","selection":{"filter":"item","values":["SSS"]}}],"response":{"format":"json-stat"}}')
-
-wdfin_list <- POST(urlfin, body = reqfin) %>%
-  content("text") %>%
-  fromJSON()
-
-wdfin <- cbind(unlist(wdfin_list$dataset$dimension$Viikko$category$label), 
-               wdfin_list$dataset$value) %>% 
-  as_tibble(data.frame()) %>%
-  mutate(year = as.integer(substr(V1, 1, 4)),
-         week = as.integer(substr(V1, 6, 7)),
-         deaths = as.integer(V2),
-         point_type = ifelse(year == 2020, "2020", "2015--2019"),
-         country = "Finland") %>%
-  select(year, week, deaths, point_type, country) %>%
-  left_join(popfin, by = "year") %>%
-  mutate(deaths_per100k = 100000 * deaths / population) %>%
-  filter(week != 53)
+# 
+# #  Finland ----
+# urlfin_pop <- "https://pxnet2.stat.fi/PXWeb/api/v1/en/StatFin/vrm/vaerak/statfin_vaerak_pxt_11ra.px"
+# reqfin_pop <- '{"query":[{"code":"Alue","selection":{"filter":"item","values":["SSS"]}},{"code":"Tiedot","selection":{"filter":"item","values":["vaesto"]}},{"code":"Vuosi","selection":{"filter":"item","values":["2014","2015","2016","2017","2018","2019"]}}],"response":{"format":"json-stat"}}'
+# 
+# popfin_list <- POST(urlfin_pop, body = reqfin_pop) %>%
+#   content("text") %>%
+#   fromJSON()
+# 
+# popfin <- cbind(unlist(popfin_list$dataset$dimension$Vuosi$category$label), 
+#                 popfin_list$dataset$value) %>% 
+#   as_tibble(data.frame()) %>%
+#   mutate(year = as.integer(V1) + 1, # 31 dec numbers, so assign to next year
+#          population = as.numeric(V2)) %>%
+#   select(year, population)
+# 
+# urlfin <- "https://pxnet2.stat.fi:443/PXWeb/api/v1/en/Kokeelliset_tilastot/vamuu_koke/statfin_vamuu_pxt_12ng.px"
+# 
+# weeks <- crossing(year = 2015:2020, week = 1:53) %>%
+#   mutate(week_fmt = paste(year, str_pad(week, width = 2, pad = "0"), sep = "W")) %>%
+#   filter(!(week == 53 & year != 2015)) %>%
+#   pull(week_fmt) %>%
+#   paste(collapse = '","')
+# 
+# reqfin <- paste0('{"query":[{"code":"Alue","selection":{"filter":"item","values":["SSS"]}},{"code":"Viikko","selection":{"filter":"item","values":["',
+#                  weeks,
+#                  '"]}},{"code":"Ikä","selection":{"filter":"item","values":["SSS"]}},{"code":"Sukupuoli","selection":{"filter":"item","values":["SSS"]}}],"response":{"format":"json-stat"}}')
+# 
+# wdfin_list <- POST(urlfin, body = reqfin) %>%
+#   content("text") %>%
+#   fromJSON()
+# 
+# wdfin <- cbind(unlist(wdfin_list$dataset$dimension$Viikko$category$label), 
+#                wdfin_list$dataset$value) %>% 
+#   as_tibble(data.frame()) %>%
+#   mutate(year = as.integer(substr(V1, 1, 4)),
+#          week = as.integer(substr(V1, 6, 7)),
+#          deaths = as.integer(V2),
+#          point_type = ifelse(year == 2020, "2020", "2015--2019"),
+#          country = "Finland") %>%
+#   select(year, week, deaths, point_type, country) %>%
+#   left_join(popfin, by = "year") %>%
+#   mutate(deaths_per100k = 100000 * deaths / population) %>%
+#   filter(week != 53)
--- nordic.R
+++ nordic.R
@@ -4,7 +4,7 @@ library(stringr)
 library(lubridate)
 library(ggplot2)
 library(readxl)
-library(thematic)
+library(thematic) # remotes::install_github("rstudio/thematic")
 
 source("get_data.R", encoding = "UTF-8")
 
@@ -14,7 +14,7 @@ thematic_on(
 )
 
 # All countries
-wdall <- bind_rows(wdnor, wdswe, wdfin, wddnk) %>%
+wdall <- bind_rows(wdnor, wdswe, wddnk) %>%
   filter(year >= 2015,
          year < 2020 | week <= isoweek(Sys.Date()) - 2)
 
@@ -45,16 +45,28 @@ ggplot(wdall, aes(x = week, y = deaths_per100k,
   theme_minimal() +
   xlab("Week") +
   ylab("Deaths per 100k") +
-  theme(legend.position = c(0.6, 0.8), plot.margin = margin(15, 5, 5, 5), 
+  theme(legend.position = c(0.6, 0.8), plot.margin = margin(15, 5, 5, 5),
         legend.box = "horizontal")
 
 # Faceted plot
-ggplot(wdall, aes(x = week, y = deaths_per100k, 
+ggplot(wdall, aes(x = week, y = deaths_per100k,
                   color = country)) +
   geom_point(aes(shape = point_type)) +
   guides(shape = guide_legend(override.aes = list(color = 8))) +
   scale_shape_manual(name = "", values = c(1, 16)) +
   scale_color_discrete(guide = "none") +
-  geom_smooth(data = filter(wdall, point_type == "2020"), se = TRUE) + 
+  geom_smooth(data = filter(wdall, point_type == "2020"), se = TRUE) +
   geom_smooth(data = filter(wdall, point_type != "2020"), linetype = "dashed", se = FALSE) +
-  facet_wrap(vars(country)) 
+  facet_wrap(vars(country))
+
+# Faceted plot with lines
+wdall %>%
+  group_by(country, point_type, week) %>%
+  summarize(deaths_per100k = mean(deaths_per100k)) %>%
+  ggplot(aes(x = week, y = deaths_per100k,
+                  color = country)) +
+  geom_line(aes(linetype = point_type)) +
+  guides(linetype = guide_legend(override.aes = list(color = 8))) +
+  scale_linetype_manual(name = "", values = c(2, 1)) +
+  scale_color_discrete(guide = "none") +
+  facet_wrap(vars(country))
